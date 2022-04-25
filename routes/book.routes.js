ee3b9e9e899b07ffbdbe03394ee911bbae421ac1
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-04-25 17:03:32 +0200

    add funcionality to display chages in a book

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -3,16 +3,16 @@ const { render, response } = require("../app");
 
 const router = require("express").Router();
 
-// create route for books list
+// route to display all books
 router.get("/books", (req, res, next) => {
 
     Book.find()
-        .then( bookArr => {
+        .then(bookArr => {
             // console.log(bookArr.length);
-            res.render("books/books-list", {books: bookArr});
+            res.render("books/books-list", { books: bookArr });
         }
         )
-        .catch( err => {
+        .catch(err => {
             console.log("error getting books from DB", err);
             next(err);
         })
@@ -33,30 +33,67 @@ router.post('/books/create', (req, res, next) => {
         rating: req.body.rating,
     }
     Book.create(newBook)
-    .then( response => {
-        console.log("New book stored successfuly", response);
-        res.redirect("/books");
-    })
-    .catch( err => {
-        console.log("error creating a new document in DB", err);
-        next(err);
-    })
+        .then(response => {
+            console.log("New book stored successfuly", response);
+            res.redirect("/books");
+        })
+        .catch(err => {
+            console.log("error creating a new document in DB", err);
+            next(err);
+        })
 
-  });
+});
+
 
 // create route for book details
 router.get("/books/:bookId", (req, res, next) => {
     const id = req.params.bookId;
     Book.findById(id)
-        .then( bookDetails => {
+        .then(bookDetails => {
             console.log(bookDetails);
-            res.render("books/book-details", {book: bookDetails});
+            res.render("books/book-details", { book: bookDetails });
         }
         )
-        .catch( err => {
+        .catch(err => {
             console.log("error getting book details from DB", err);
             next(err);
         })
 });
+
+// display form to update books
+router.get("/books/:bookId/edit", (req, res, next) => {
+    const id = req.params.bookId;
+    Book.findById(id)
+        .then(bookDetails => {
+            res.render("books/book-edit", bookDetails)
+        })
+        .catch(err => {
+            console.log("error reading a book", err);
+            next(err);
+        })
+})
+
+
+
+// request to update books
+router.post("/books/:bookId/edit", (req, res, next) => {
+    const id = req.params.bookId;
+    const newDetails = {
+        title: req.body.title,
+        description: req.body.description,
+        author: req.body.author,
+        rating: req.body.rating,
+    };
+
+    Book.findByIdAndUpdate(id, newDetails)
+    .then(response => {
+        console.log(response);
+        res.redirect(`/books/${response._id}`)
+    })
+    .catch(err => {
+        console.log("error updating book", err);
+        next(err);
+    })
+})
 
 module.exports = router;
