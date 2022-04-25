083079970040dd63b0c1e728ad58c03aaf0044df
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-04-25 14:50:34 +0200

    create route for book details and new view for add books

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -3,6 +3,7 @@ const { render, response } = require("../app");
 
 const router = require("express").Router();
 
+// create route for books list
 router.get("/books", (req, res, next) => {
 
     Book.find()
@@ -17,5 +18,21 @@ router.get("/books", (req, res, next) => {
         })
 });
 
+// create route for book details
+router.get("/books/:bookId", (req, res, next) => {
+
+    const id = req.params.bookId;
+
+    Book.findById(id)
+        .then( bookDetails => {
+            console.log(bookDetails);
+            res.render("books/book-details", {book: bookDetails});
+        }
+        )
+        .catch( err => {
+            console.log("error getting book details from DB", err);
+            next(err);
+        })
+});
 
 module.exports = router;
